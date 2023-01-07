caa5e5743ee3b21d09ffd37f12fd9f9ce20195a0
yakisova41 <yakisoba5emon@gmail.com>
2023-01-07 03:08:55 +0000

    a

--- build/build.js
+++ build/build.js
@@ -1,81 +1,89 @@
-module.exports = (mode)=>{
-    const esbuild = require('esbuild');
-    const fs = require('fs');
-    const path = require('path');
-    const writeUserscriptHeader = require('./writeUserscriptHeader.js')
-    const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '/../package.json')));
+module.exports = (mode) => {
+    const esbuild = require("esbuild");
+    const fs = require("fs");
+    const path = require("path");
+    const writeUserscriptHeader = require("./writeUserscriptHeader.js");
+    const packageJson = JSON.parse(
+        fs.readFileSync(path.join(__dirname, "/../package.json"))
+    );
+    const cssModulesPlugin = require("esbuild-ssr-css-modules-plugin").default;
+    const eslint = require("esbuild-plugin-eslint");
 
     /**
      * dev and production common plugins
      */
-    const plugins = [];
+    const plugins = [
+        cssModulesPlugin({
+            jsCSSInject: true,
+        }),
+        eslint(),
+    ];
 
-    switch(mode){
-        case 'build':
-            (()=>{
+    switch (mode) {
+        case "build":
+            (() => {
                 const config = {
                     logLevel: "info",
-                    entryPoints: [
-                        path.join(__dirname, '/../src' , 'index.ts')
-                    ],
+                    entryPoints: [path.join(__dirname, "/../src", "index.ts")],
                     define: {
-                        'process.env.NODE_ENV': "'production'",
+                        "process.env.NODE_ENV": "'production'",
                     },
-                    outfile: path.join(__dirname, '/../dist' , 'index.user.js'),
+                    outfile: path.join(__dirname, "/../dist", "index.user.js"),
                     bundle: true,
-                    plugins:[...plugins, writeUserscriptHeader()]
+                    plugins: [...plugins, writeUserscriptHeader()],
                 };
 
-                if(packageJson.userScript?.esbuild !== undefined){
-                    Object.keys(packageJson.userScript.esbuild).forEach((key)=>{
-                        config[key] = packageJson.userScript.esbuild[key];
-                    });
+                if (packageJson.userScript?.esbuild !== undefined) {
+                    Object.keys(packageJson.userScript.esbuild).forEach(
+                        (key) => {
+                            config[key] = packageJson.userScript.esbuild[key];
+                        }
+                    );
                 }
 
-                esbuild.build(config)
-                    .catch(() => process.exit(1));             
-            })()
+                esbuild.build(config).catch(() => process.exit(1));
+            })();
 
             break;
 
-        case 'dev':
-            (()=>{
+        case "dev":
+            (() => {
                 const hotReloadCliantconfig = {
-                    entryPoints: [
-                        path.join(__dirname, 'cliant' , 'index.ts')
-                    ],
-                    outfile: path.join(__dirname, '/../dist' , 'index.user.js'),
+                    entryPoints: [path.join(__dirname, "cliant", "index.ts")],
+                    outfile: path.join(__dirname, "/../dist", "index.user.js"),
                     bundle: true,
-                    plugins:[writeUserscriptHeader()]
+                    plugins: [writeUserscriptHeader()],
                 };
 
                 const devConfig = {
                     logLevel: "info",
-                    entryPoints: [
-                        path.join(__dirname, '/../src' , 'index.ts') 
-                    ],
-                    outfile: path.join(__dirname, 'tmp' , 'dev.user.js'),
+                    entryPoints: [path.join(__dirname, "/../src", "index.ts")],
+                    outfile: path.join(__dirname, "tmp", "dev.user.js"),
                     bundle: true,
-                    watch:true,
-                    plugins:plugins
+                    watch: true,
+                    plugins: plugins,
                 };
 
-                if(packageJson.userScript?.esbuild !== undefined){
-                    Object.keys(packageJson.userScript.esbuild).forEach((key)=>{
-                        if(key !== 'entryPoints') {
-                            hotReloadCliantconfig[key] = packageJson.userScript.esbuild[key];
+                if (packageJson.userScript?.esbuild !== undefined) {
+                    Object.keys(packageJson.userScript.esbuild).forEach(
+                        (key) => {
+                            if (key !== "entryPoints") {
+                                hotReloadCliantconfig[key] =
+                                    packageJson.userScript.esbuild[key];
+                            }
+                            devConfig[key] =
+                                packageJson.userScript.esbuild[key];
                         }
-                        devConfig[key] = packageJson.userScript.esbuild[key];
-                    });
+                    );
                 }
 
-                esbuild.build(hotReloadCliantconfig)
+                esbuild
+                    .build(hotReloadCliantconfig)
                     .catch(() => process.exit(1))
-                    .then(()=>{
-                        esbuild.build(devConfig)
-                            .catch(() => process.exit(1))
+                    .then(() => {
+                        esbuild.build(devConfig).catch(() => process.exit(1));
                     });
-            })()
+            })();
 
             break;
     }
