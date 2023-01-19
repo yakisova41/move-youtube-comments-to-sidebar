1e95bae1442a61790112c3b8bc54abc0b691da5f
yakisova41 <yakisoba5emon@gmail.com>
2023-01-19 08:56:34 +0000

    0.1

--- src/ElementManager.ts
+++ src/ElementManager.ts
@@ -73,7 +73,7 @@ export class ElementManager {
                     //pass
                 }
             });
-        });
+        }, 100);
     }
 
     /**
--- src/domInitialize.ts
+++ src/domInitialize.ts
@@ -21,7 +21,10 @@ function createCommentToggleBtn(manager: ElementManager) {
         commentToggleBtn.innerHTML +
         `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><path d="M8,7h8v2H8V7z M8,13h5v-2H8V13z M5,3v13h10h0.41l0.29,0.29L19,19.59V3H5 M4,2h16v20l-5-5H4V2L4,2z"></path></svg>`;
 
-    manager.appendChild("ytd-menu-renderer", commentToggleBtn);
+    manager.appendChild(
+        "ytd-menu-renderer.ytd-watch-metadata",
+        commentToggleBtn
+    );
 }
 
 /**
